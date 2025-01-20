3db2096275e557a2c1477868f23efb515fb2efb6
Luhruk <87099954+Luhruk@users.noreply.github.com>
2025-01-20 03:15:25 -0700

    Update reporter.py

--- reporter/reporter.py
+++ reporter/reporter.py
@@ -6,15 +6,14 @@ from discord.ext.commands import has_permissions
 class Reporter(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.forum_channel = None  # Will hold the forum channel for creating threads
-        self.role_name = None  # Will hold the role name for pinging moderators
-        self.locked_threads = {}  # Dictionary to track locked threads
+        self.forum_channel = None
+        self.role_name = None
+        self.locked_threads = {}
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.guild is None and not message.author.bot:  # This is a direct message
+        if message.guild is None and not message.author.bot:
             if self.locked_threads.get(message.author.id):
-                # Create a new thread if locked
                 await self.create_forum_thread(message.author)
             else:
                 await self.forward_message_to_thread(message)
@@ -22,32 +21,31 @@ class Reporter(commands.Cog):
     @commands.command()
     @has_permissions(administrator=True)
     async def reporter_set_forum_channel(self, ctx: commands.Context, channel: discord.TextChannel):
-        """Sets the forum channel where threads will be created."""
         self.forum_channel = channel
         await ctx.send(f"Forum channel set to {channel.mention}")
+        print(f"[DEBUG] Forum channel set to: {channel.name} ({channel.id})")
 
     @commands.command()
     @has_permissions(manage_roles=True)
     async def reporter_set_role(self, ctx: commands.Context, role: discord.Role):
-        """Sets the role to be pinged when a new thread is created."""
         self.role_name = role.name
         await ctx.send(f"Role for notifications set to {role.name}")
+        print(f"[DEBUG] Role set to: {role.name} ({role.id})")
 
     async def create_forum_thread(self, user: discord.User):
-        """Creates a forum thread when a DM is received."""
         if not self.forum_channel:
             return await user.send("No forum channel has been set by the moderators.")
 
-        # Create a forum thread
+        print(f"[DEBUG] Creating thread for user: {user.name} ({user.id})")
+
         thread = await self.forum_channel.create_text_channel(
             name=f"{user.name}-{user.id}",
             overwrites={
                 self.forum_channel.guild.default_role: discord.PermissionOverwrite(view_channel=False),
-                user: discord.PermissionOverwrite(view_channel=True)
-            }
+                user: discord.PermissionOverwrite(view_channel=True),
+            },
         )
 
-        # Create the embed with the user's profile information
         embed = discord.Embed(
             title=f"User Profile - {user.name}",
             description=(
@@ -56,35 +54,29 @@ class Reporter(commands.Cog):
                 f"Joined Server: {user.joined_at.strftime('%Y-%m-%d %H:%M:%S')}\n"
                 f"Roles: {', '.join([role.name for role in user.roles if role.name != '@everyone'])}"
             ),
-            color=discord.Color.blue()
+            color=discord.Color.blue(),
         )
         embed.set_thumbnail(url=user.avatar.url if user.avatar else None)
         await thread.send(embed=embed)
 
-        # Ping the specified role
         if self.role_name:
             role = discord.utils.get(self.forum_channel.guild.roles, name=self.role_name)
             if role:
                 await thread.send(f"Ping: {role.mention}")
+                print(f"[DEBUG] Pinged role: {role.name} ({role.id})")
 
-        # Store the thread for future message forwarding
         self.locked_threads[user.id] = thread
 
     async def forward_message_to_thread(self, message: discord.Message):
-        """Forwards the user's message to the appropriate forum thread."""
         thread = self.locked_threads.get(message.author.id)
         if thread:
-            embed = discord.Embed(
-                description=message.content,
-                color=discord.Color.green()
-            )
+            embed = discord.Embed(description=message.content, color=discord.Color.green())
             embed.set_footer(text=f"User ID: {message.author.id}")
             await thread.send(embed=embed)
 
     @commands.command()
     @has_permissions(administrator=True)
     async def reporter_close(self, ctx: commands.Context):
-        """Closes a thread by locking it."""
         if ctx.channel.id in [t.id for t in self.locked_threads.values()]:
             self.locked_threads[ctx.channel.id] = True
             await ctx.send("Thread has been closed and locked.")
@@ -93,7 +85,6 @@ class Reporter(commands.Cog):
 
     @commands.command()
     async def reporter_reopen(self, ctx: commands.Context):
-        """Reopens a locked thread."""
         if ctx.channel.id in self.locked_threads and self.locked_threads[ctx.channel.id]:
             self.locked_threads[ctx.channel.id] = False
             await ctx.send("Thread has been reopened.")
@@ -103,3 +94,4 @@ class Reporter(commands.Cog):
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Reporter(bot))
+    print("[DEBUG] Reporter cog loaded successfully.")
