a5f7e041c9131e02266bb5e9fd8bb23278c2f966
Luhruk <87099954+Luhruk@users.noreply.github.com>
2025-01-20 02:50:32 -0700

    Update __init__.py

--- reporter/__init__.py
+++ reporter/__init__.py
@@ -1,4 +1,12 @@
+import logging
 from .reporter import Reporter
 
+log = logging.getLogger(__name__)
+
 async def setup(bot):
-    await bot.add_cog(Reporter(bot))
+    try:
+        log.info("Attempting to add Reporter cog...")
+        await bot.add_cog(Reporter(bot))
+        log.info("Reporter cog added successfully.")
+    except Exception as e:
+        log.error(f"Error loading Reporter cog: {e}")
