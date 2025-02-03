e1daf48a8a2cb0301450f8486bab7866e453f7a8
Luhruk <87099954+Luhruk@users.noreply.github.com>
2025-02-03 16:54:46 -0700

    Delete rpg directory

--- rpg/spells.py
+++
@@ -1,9 +0,0 @@
-class Spells:
-    def __init__(self, data_handler):
-        self.data = data_handler.load_spell_data()
-
-    def get_spell(self, spell_name):
-        return self.data.get(spell_name, "Spell not found!")
-
-    def list_spells(self):
-        return list(self.data.keys())
--- rpg/world.py
+++
@@ -1,9 +0,0 @@
-class World:
-    def __init__(self, data_handler):
-        self.data = data_handler.load_world_data()
-
-    def get_location(self, location_id):
-        return self.data.get(location_id, "Location not found!")
-
-    def list_locations(self):
-        return list(self.data.keys())
